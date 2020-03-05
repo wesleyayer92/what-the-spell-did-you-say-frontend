e8f547a56d4eb02019d120215285dddf6caf62ac
Casey Fayard <fayardcasey@gmail.com>
2020-03-05 13:27:07 -0500

    pushing code for modal component

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,34 @@
 import React from 'react'
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, View, Modal, Button } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-
+import HomeModal from './HomeModal'
 const Home = () => {
    const goToQuiz = () => {
       Actions.quiz()
    }
+
+const _Instructions = () => {
+   return 'Blue'
+}
    return (
-      <TouchableOpacity style = {{ margin: 128 }} onPress = {goToQuiz}>
-         <Text>This is HOME!</Text>
-      </TouchableOpacity>
+      
+      <View style={{flex: 1}}>
+      
+    
+
+      <View >
+         <View style= {{color: 'red', backgroundColor:'black'}}>
+            <TouchableOpacity style = {{ margin: 128 }} onPress = {goToQuiz}>
+               <Text style={{color: 'yellow'}}>Welcome to the Spelling Game</Text>
+            </TouchableOpacity>
+         </View>
+           <Button title='Instructions'onPress={() => {_Instructions; }}>
+               Instructions
+            </Button>
+      </View>
+      <HomeModal />
+      </View>
+      
    )
 }
 export default Home
