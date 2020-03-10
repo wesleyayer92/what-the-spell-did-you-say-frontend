d8b8c2ab649696b06d832309c1f530e386f41d96
wes <wesleyayer92@gmail.com>
2020-03-10 14:22:02 -0400

    get word array on componentdidmount and show quiz score in scorecard

--- TextToVoice.js
+++ TextToVoice.js
@@ -2,25 +2,33 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export default function TextToVoice({sayWord, sayDefinition, sayPartOfSpeech, makeApiRequest}) {
-    return (
-      <View>
-        <Text style={[styles.chalky, {textAlign: 'center', fontSize: 40, textDecorationLine: 'none'}]}>{`Hello, class! :)\n`}</Text>
-        
-        <TouchableOpacity onPress={sayWord}>
-          <Text style={styles.chalky}>1. Word</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={sayDefinition}>
-          <Text style={styles.chalky}>2. Definition</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={sayPartOfSpeech}>
-          <Text style={styles.chalky}>3. Part of Speech</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={makeApiRequest}>
-          <Text style={styles.chalky}>4. Make Api Request</Text>
-        </TouchableOpacity>
-      </View>
-    );
+export default class TextToVoice extends React.Component {
+    // makeApiRequest();
+    componentDidMount() {
+      this.props.makeApiRequest();
+    }
+    // ({sayWord, sayDefinition, sayPartOfSpeech, makeApiRequest})
+
+    render() {
+      return (
+        <View>
+          <Text style={[styles.chalky, {textAlign: 'center', fontSize: 40, textDecorationLine: 'none'}]}>{`Hello, class! :)\n`}</Text>
+          
+          <TouchableOpacity onPress={this.props.sayWord}>
+            <Text style={styles.chalky}>1. Word</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.props.sayDefinition}>
+            <Text style={styles.chalky}>2. Definition</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.props.sayPartOfSpeech}>
+            <Text style={styles.chalky}>3. Part of Speech</Text>
+          </TouchableOpacity>
+          {/* <TouchableOpacity onPress={makeApiRequest}>
+            <Text style={styles.chalky}>4. Make Api Request</Text>
+          </TouchableOpacity> */}
+        </View>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
